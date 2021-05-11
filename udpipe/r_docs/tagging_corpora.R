d88f8e029632adc1e163c21efeaa98433ce7ea3d
Tim Marchand <timmarchand@gmail.com>
2021-05-11 20:00:41 +0900

    modified ngram selection process

--- udpipe/r_docs/tagging_corpora.R
+++ udpipe/r_docs/tagging_corpora.R
@@ -80,8 +80,8 @@ bnc_comments <- comments %>%
 
 ## save as rdata
 
-saveRDS(bnc_threads, file = here("udpipe","data", "bnc_threads.rds"))
-saveRDS(bnc_comments, file = here("udpipe","data", "bnc_comments.rds"))
+saveRDS(bnc_threads, file = here("ignored_files", "bnc_threads.rds"))
+saveRDS(bnc_comments, file = here("ignored_files", "bnc_comments.rds"))
 
 # Zip and save ------------------------------------------------------------
 
@@ -266,10 +266,38 @@ files <- list_paths %>% basename %>% path_ext_remove()
 
 # Getting ngrams ----------------------------------------------------------
 
-library(tidytext)    
-
-all_threads <- bind_rows(joc_threads, hoc_threads, bnc_threads)
+library(tidytext)   
+library(tidylog)
     
+## hoc_threads and joc_threads can be found in udpipe/data
+## bnc_threads is in ignored_files
+    
+# bnc_threads <- read_rds(here("ignored_files","bnc_threads.rds"))
+# hoc_threads <- read_rds(here("udpipe","data","hoc_threads.rds"))
+# joc_threads <- read_rds(here("udpipe","data","joc_threads.rds"))
+
+all_threads <- bind_rows(joc_threads, hoc_threads, bnc_threads) %>% 
+                mutate(wc = str_count(.$tags_only, "\\b\\w+\\b"),
+                       corpus = str_extract(.$file, "^..."), .after = file) 
+all_threads %>% group_by(corpus) %>% 
+  summarise(total_wc = sum(wc))
+
+all_threads %>% ggplot(aes(wc)) +
+  geom_histogram() +
+  facet_wrap(~corpus, scales  = "free") +
+  scale_x_continuous(trans = "log10") +
+  theme_light()
+
+all_threads %>% filter(wc >500) %>% 
+  group_by(corpus) %>% 
+  summarise(total_wc = sum(wc))
+
+all_threads %>% filter(wc >500) %>% 
+  count(corpus)
+    
+threads_wc100 <- all_threads %>% filter(wc >100)
+threads_wc500 <- all_threads %>% filter(wc >500)
+
 ngrams <- all_threads %>% select(file,tags_only) %>% 
      # remove commas, periods, colons and semi-colons as punctuation
      mutate(tags_only = map_chr(tags_only, ~str_replace_all(.x, "[,\\.:;]", "BREAK"))) %>% 
@@ -279,9 +307,38 @@ ngrams <- all_threads %>% select(file,tags_only) %>%
       mutate(trigram = str_replace_all(.$trigram,' ','_')) %>% 
      # count the number per file
      count(file, trigram) 
+
+ngrams_wc100 <- threads_wc100 %>% select(file,tags_only) %>% 
+  # remove commas, periods, colons and semi-colons as punctuation
+  mutate(tags_only = map_chr(tags_only, ~str_replace_all(.x, "[,\\.:;]", "BREAK"))) %>% 
+  unnest_tokens(output = trigram,input = tags_only, token = "ngrams",n = 3) %>% ungroup() %>% 
+  # to avoid trigrams overlapping clause boundaries
+  filter(!str_detect(trigram, "break")) %>% 
+  mutate(trigram = str_replace_all(.$trigram,' ','_')) %>% 
+  # count the number per file
+  count(file, trigram) 
+
+
+ngrams_wc500 <- threads_wc500 %>% select(file,tags_only) %>% 
+  # remove commas, periods, colons and semi-colons as punctuation
+  mutate(tags_only = map_chr(tags_only, ~str_replace_all(.x, "[,\\.:;]", "BREAK"))) %>% 
+  unnest_tokens(output = trigram,input = tags_only, token = "ngrams",n = 3) %>% ungroup() %>% 
+  # to avoid trigrams overlapping clause boundaries
+  filter(!str_detect(trigram, "break")) %>% 
+  mutate(trigram = str_replace_all(.$trigram,' ','_')) %>% 
+  # count the number per file
+  count(file, trigram) 
      
 ## PIVOT WIDER
 full_pivot <- ngrams %>% 
+  pivot_wider(names_from =trigram, values_from = n, values_fill = 0) %>% 
+  mutate(corpus = str_extract(.$file, "^..."), .after = file)
+
+wc100_pivot <- ngrams_wc100 %>% 
+  pivot_wider(names_from =trigram, values_from = n, values_fill = 0) %>% 
+  mutate(corpus = str_extract(.$file, "^..."), .after = file)
+
+wc500_pivot <- ngrams_wc500 %>% 
   pivot_wider(names_from =trigram, values_from = n, values_fill = 0) %>% 
   mutate(corpus = str_extract(.$file, "^..."), .after = file)
 
@@ -289,5 +346,60 @@ trigram_totals <- full_pivot %>% group_by(corpus) %>%
   summarise(across(where(is.integer), ~ sum((.x))))  %>% 
   pivot_longer(cols = -corpus, names_to = "trigram", values_to = "total") 
 
+trigram_wc100_totals <- wc100_pivot %>% group_by(corpus) %>% 
+  summarise(across(where(is.integer), ~ sum((.x))))  %>% 
+  pivot_longer(cols = -corpus, names_to = "trigram", values_to = "total") 
 
-trigram_totals %>% filter(total >2000) %>% filter(corpus == "dem")
+
+
+trigram_totals %>% group_by(corpus) %>% mutate(overall = sum(total)) %>% 
+  ungroup() %>% 
+  mutate(proportion = total/overall*1000) %>% 
+  filter(proportion>2) %>% 
+  tally()
+
+# get the top 50 trigrams for each corpus
+trigram_wc100_totals %>% group_by(corpus) %>% dplyr::slice_max(total,n = 50) %>% pull(trigram) -> top_50
+top_50 %>% unique -> trigrams_efa
+
+all_threads %>% count(corpus) -> all_count
+
+threads_wc100 %>% count(corpus)
+
+all_threads %>% filter(wc >500) %>% count(corpus) -> wc500_count
+
+change <- tibble(corpus = all_count$corpus,
+                 removed = all_count$n - wc500_count$n)
+
+wc500_count %>% summarise(sum = sum(n))
+
+
+## selecting trigrams in full pivot table
+full_pivot %>% select(file, corpus, one_of(trigrams_efa))
+
+## check if any are dropped by filtering for low proportions
+trigram_totals %>% filter(trigram %in% trigrams_efa) %>% filter(total > 0) # tidylog result: none removed
+
+## check by proportion one per 1000
+trigram_totals %>% group_by(corpus) %>% mutate(overall = sum(total)) %>% 
+  ungroup() %>% 
+  mutate(proportion = total/overall*1000) %>% 
+  filter(trigram %in% trigrams_efa) %>%
+  filter(proportion>1) # tidylog result: 163 removed (22%)
+
+## check by proportion one per 10000
+trigram_totals %>% group_by(corpus) %>% mutate(overall = sum(total)) %>% 
+  ungroup() %>% 
+  mutate(proportion = total/overall*10000) %>% 
+  filter(trigram %in% trigrams_efa) %>%
+  filter(proportion>1) # tidylog result: 8 removed (1%)
+
+files_less_500 <- all_threads %>% filter(wc <= 500) %>% pull(file)
+
+
+## Use %notin% function
+`%notin%` <- Negate(`%in%`)
+
+full_pivot %>% filter(file %notin% files_less_500 ) %>% 
+  select(file, corpus, one_of(trigrams_efa))
+
